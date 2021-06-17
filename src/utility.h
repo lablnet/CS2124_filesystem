5596455502493f7abe110c99b9d68df045753f15
Muhammad Umer Farooq <mumerfarooqlablnet01@gmail.com>
2021-06-17 10:51:25 +0500

    update

--- src/utility.h
+++ src/utility.h
@@ -27,30 +27,27 @@ namespace lablnet {
 
     std::string get_file_name(std::string path)
     {
-        std::vector<std::string> tokens = split_string(path, '.');
+        std::vector<std::string> tokens = lablnet::split_string(path, '.');
         std::string name = tokens[tokens.size() - 2];
-        std::vector<std::string> tokens2 = split_string(name, '/');
+        std::vector<std::string> tokens2 = lablnet::split_string(name, '/');
         return tokens2[tokens.size() - 1];
     }
 
     std::string get_file_ext(std::string path)
     {
-        std::vector<std::string>  tokens = split_string(path, '.');
+        std::vector<std::string>  tokens = lablnet::split_string(path, '.');
         return tokens[tokens.size() - 1];
     }
 
     std::string get_path_name(std::string path)
     {
-        std::vector<std::string>  tokens = split_string(path, '/');
+        std::vector<std::string>  tokens = lablnet::split_string(path, '/');
         return tokens[tokens.size() - 1];
     }
 
-    long long int get_size(std::string path)
+    std::string run_command(std::string cmd)
     {
-        long long result = -1;
-
-        // Command to get size.
-        std::string cmd = "du -sb " + path + " | cut -f1";
+        std::string result;
 
         // Run the command and get the output
         FILE *pipe = popen(cmd.c_str(), "r");
@@ -58,11 +55,20 @@ namespace lablnet {
             char buffer[256];
             while (!std::feof(pipe)) {
                 if (fgets(buffer, 256, pipe) != nullptr)
-                    result = result + std::atoll(buffer);
+                    result = result + buffer;
             }
             pclose(pipe);
         }
         return result;
+    }
+
+    long long int get_size(std::string path) {
+        // std::stoll convert std::string into long.
+        return std::stoll(lablnet::run_command("du -sb " + path + " | cut -f1"));
+    }
+
+    std::string get_last_modified(std::string path) {
+        return lablnet::run_command("date -r " + path);
     }
 }
 
