cf9c1104da75e21335b16ae0fdbb82d2840497b6
Muhammad Umer Farooq <mumerfarooqlablnet01@gmail.com>
2021-06-20 10:06:41 +0500

    update

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include "./src/Disk/Disk.h"
 
 int main() {
+    lablnet::Disk disk;
+    disk.copy("./src", "./temp/src");
+
+
     std::cout << "Hello, AlphaFileSystem!" << std::endl;
     return 0;
 }
--- src/Disk/Disk.h
+++ src/Disk/Disk.h
@@ -7,6 +7,7 @@
 
 #include "../utility.h"
 #include "../HashTable/HashTable.cpp"
+#include "../Mime/Mime.h"
 #include <filesystem>
 #include <string>
 
@@ -17,30 +18,52 @@ namespace lablnet {
         lablnet::FileType type = lablnet::NONE;
         std::string name;
         std::string last_mod;
-        long long size; // in bytes.
+        long long int size; // in bytes.
         std::string mime;
         std::string extension;
-
-
     };
 
     class Disk {
     private:
-        HashTable<std::string, FileMeta> table;
-        struct stat s;
+        HashTable<std::string, lablnet::FileMeta> table;
     public:
+
         void copy(std::string from, std::string to) {
-            std::filesystem::copy(from, to);
-            // get file/folder name and size.
-            // and other meta too.
+            lablnet::FileType type = lablnet::get_type(from);
+            if (type != lablnet::NONE) {
+                // std::filesystem::copy(from, to);
+                std::string filename = lablnet::get_path_name(from);
+                long long int size = lablnet::get_size(from);
+                std::string ext = lablnet::get_file_ext(from);
+                std::string last_mod = lablnet::get_last_modified(from);
+                auto *meta = new lablnet::FileMeta();
+                meta->size = size;
+                meta->type = type;
+                meta->name = filename;
+                meta->last_mod = last_mod;
+                if (type == lablnet::_FILE) {
+                    meta->extension = lablnet::get_file_ext(from);
+                    meta->mime = MimeTypes::get(meta->extension);
+                }
+               this->table.insert(filename, *meta);
+            }
         }
 
         void move() {
             // todo
         }
 
-        void remove() {
-            // todo
+        void remove(std::string path) {
+            lablnet::FileType type = lablnet::get_type(path);
+            if (type != lablnet::NONE) {
+                std::string filename = lablnet::get_file_name(path);
+                this->table.erase(filename);
+            }
+        }
+
+        void printDiskTable()
+        {
+
         }
 
         void getInfo() {
